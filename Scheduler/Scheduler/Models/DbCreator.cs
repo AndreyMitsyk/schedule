a58df030983529a4140320879f06eae1acf016df
Kroniya <kropacheva_kseniya@mail.ru>
2014-11-23 17:59:34 +0300

    Добавлена анимация для меню пользователя.

--- Scheduler/Scheduler/Models/DbCreator.cs
+++ Scheduler/Scheduler/Models/DbCreator.cs
@@ -17,81 +17,81 @@ namespace Scheduler.Models
         {
             Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Db>());
 
-            using (var db = new Db())
-            {
-                if (!db.Auditoriums.Any())
-                {
-                    foreach (var s in GetFileData("Auditoriums"))
-                    {
-                        db.Auditoriums.Add(new Auditorium { Name = s });
-                    }
-                }
-                if (!db.Courses.Any())
-                {
-                    foreach (var s in GetFileData("Courses"))
-                    {
-                        db.Courses.Add(new Course { Name = s });
-                    }
-                }
-                if (!db.DayOfWeekItems.Any())
-                {
-                    foreach (var s in GetFileData("DayOfWeekItems"))
-                    {
-                        db.DayOfWeekItems.Add(new DayOfWeekItem { Name = s });
-                    }
-                }
-                if (!db.Faculties.Any())
-                {
-                    foreach (var s in GetFileData("Faculties"))
-                    {
-                        db.Faculties.Add(new Faculty { Name = s });
-                    }
-                }
-                if (!db.Groups.Any())
-                {
-                    foreach (var s in GetFileData("Groups"))
-                    {
-                        db.Groups.Add(new Group { Name = s });
-                    }
-                }
-                if (!db.LessonTimes.Any())
-                {
-                    foreach (var s in GetFileData("LessonTimes"))
-                    {
-                        db.LessonTimes.Add(new LessonTime { Name = s });
-                    }
-                }
-                if (!db.LessonTypes.Any())
-                {
-                    foreach (var s in GetFileData("LessonTypes"))
-                    {
-                        db.LessonTypes.Add(new LessonType { Name = s });
-                    }
-                }
-                if (!db.Subjects.Any())
-                {
-                    foreach (var s in GetFileData("Subjects"))
-                    {
-                        db.Subjects.Add(new Subject { Name = s });
-                    }
-                }
-                if (!db.Teachers.Any())
-                {
-                    foreach (var s in GetFileData("Teachers"))
-                    {
-                        db.Teachers.Add(new Teacher { Name = s });
-                    }
-                }
-                if (!db.WeekNumbers.Any())
-                {
-                    foreach (var s in GetFileData("WeekNumbers"))
-                    {
-                        db.WeekNumbers.Add(new WeekNumber { Name = s });
-                    }
-                }
+            //using (var db = new Db())
+            //{
+            //    if (!db.Auditoriums.Any())
+            //    {
+            //        foreach (var s in GetFileData("Auditoriums"))
+            //        {
+            //            db.Auditoriums.Add(new Auditorium { Name = s });
+            //        }
+            //    }
+            //    if (!db.Courses.Any())
+            //    {
+            //        foreach (var s in GetFileData("Courses"))
+            //        {
+            //            db.Courses.Add(new Course { Name = s });
+            //        }
+            //    }
+            //    if (!db.DayOfWeekItems.Any())
+            //    {
+            //        foreach (var s in GetFileData("DayOfWeekItems"))
+            //        {
+            //            db.DayOfWeekItems.Add(new DayOfWeekItem { Name = s });
+            //        }
+            //    }
+            //    if (!db.Faculties.Any())
+            //    {
+            //        foreach (var s in GetFileData("Faculties"))
+            //        {
+            //            db.Faculties.Add(new Faculty { Name = s });
+            //        }
+            //    }
+            //    if (!db.Groups.Any())
+            //    {
+            //        foreach (var s in GetFileData("Groups"))
+            //        {
+            //            db.Groups.Add(new Group { Name = s });
+            //        }
+            //    }
+            //    if (!db.LessonTimes.Any())
+            //    {
+            //        foreach (var s in GetFileData("LessonTimes"))
+            //        {
+            //            db.LessonTimes.Add(new LessonTime { Name = s });
+            //        }
+            //    }
+            //    if (!db.LessonTypes.Any())
+            //    {
+            //        foreach (var s in GetFileData("LessonTypes"))
+            //        {
+            //            db.LessonTypes.Add(new LessonType { Name = s });
+            //        }
+            //    }
+            //    if (!db.Subjects.Any())
+            //    {
+            //        foreach (var s in GetFileData("Subjects"))
+            //        {
+            //            db.Subjects.Add(new Subject { Name = s });
+            //        }
+            //    }
+            //    if (!db.Teachers.Any())
+            //    {
+            //        foreach (var s in GetFileData("Teachers"))
+            //        {
+            //            db.Teachers.Add(new Teacher { Name = s });
+            //        }
+            //    }
+            //    if (!db.WeekNumbers.Any())
+            //    {
+            //        foreach (var s in GetFileData("WeekNumbers"))
+            //        {
+            //            db.WeekNumbers.Add(new WeekNumber { Name = s });
+            //        }
+            //    }
 
-                db.SaveChanges();
-            }
+            //    db.SaveChanges();
+            //}
         }
     }
 }
