7878bb7fa29b43cc4192f98b7f077be4d4f86ac9
aq <aqs.dev@gmail.com>
2014-11-23 20:32:06 +0300

    Some forgotten files

---
+++ Scheduler/Scheduler/Models/SearchModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Scheduler.Models
+{
+    public class SearchModel
+    {
+        public int FacultyId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public int GroupId { get; set; }
+
+        public int WeekNumberId { get; set; }
+
+        public int DayOfWeekItemId { get; set; }
+    }
+}
---
+++ Scheduler/Scheduler/Views/Home/ExtensionMethods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Scheduler.Views.Home
+{
+    public static class ExtensionMethods
+    {
+        public static TResult Do<T, TResult>(this T o, Func<T, TResult> func, TResult defaultValue = default (TResult))
+        {
+            return !ReferenceEquals(o, null) && func != null ? func(o) : defaultValue;
+        }
+    }
+}
